a3b3e250c5d6b1bc9f633667cc9945d0c63778a7
ShivshankarGoit <136622561+ShivshankarGoit@users.noreply.github.com>
2024-07-07 15:51:54 +0530

    Delete BillGenerater directory

--- BillGenerater/BillGenerater/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BillGenerater.Data
-{
-    public class ApplicationDbContext :  IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>) { }
-    }
-    public DbSet<Customer> Customers {  get; set; }
-    public DbSet<Item> Item {  get; set; }
-}
--- BillGenerater/BillGenerater/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BillGenerater.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
